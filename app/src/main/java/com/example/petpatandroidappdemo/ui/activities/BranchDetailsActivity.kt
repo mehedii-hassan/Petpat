ebb89aecb6928cc36c33c75f23b860617ad41eb8
Mehedi Hassan <mehedi.github@gmail.com>
2023-05-22 18:05:55 +0600

    updated branch details fragment design

---
+++ app/src/main/java/com/example/petpatandroidappdemo/ui/activities/BranchDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.petpatandroidappdemo.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petpatandroidappdemo.R
+
+class BranchDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_branch_details)
+    }
+}
