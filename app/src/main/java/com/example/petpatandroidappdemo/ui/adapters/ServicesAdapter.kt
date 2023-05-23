72cdc9a2344e1b9a5b890a85dc1be4e44a018d57
Mehedi Hassan <mehedi.github@gmail.com>
2023-05-23 13:44:34 +0600

    updated

---
+++ app/src/main/java/com/example/petpatandroidappdemo/ui/adapters/ServicesAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.petpatandroidappdemo.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.petpatandroidappdemo.databinding.RvServicesItemDesignBinding
+
+class ServicesAdapter(private val list: ArrayList<Int>) :
+    RecyclerView.Adapter<ServicesAdapter.ServicesViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServicesViewHolder {
+        val binding = RvServicesItemDesignBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return ServicesViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ServicesViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    inner class ServicesViewHolder(private val binding: RvServicesItemDesignBinding) :
+        ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.ivServicesItem.setImageResource(list[position])
+
+        }
+
+    }
+    /*inner class PopularProductViewHolder(private val binding: RvFeatureProductsItemDesignBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(position: Int) {
+            binding.ivFeatureProductItem.setImageResource(featureProductImageList[position])
+
+
+        }
+    }*/
+}
