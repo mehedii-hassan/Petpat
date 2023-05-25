7ff5dc6f08b472dffc6c7ef6893a6a8ee615d78d
Mehedi Hassan <mehedi.github@gmail.com>
2023-05-25 16:38:07 +0600

    updated design

---
+++ app/src/main/java/com/example/petpatandroidappdemo/ui/fragments/dialogfragments/ProductDetailsDialogFragment.kt
@@ -0,0 +1,24 @@
+package com.example.petpatandroidappdemo.ui.fragments.dialogfragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.petpatandroidappdemo.R
+
+
+class ProductDetailsDialogFragment : DialogFragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_details_dialog, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/petpatandroidappdemo/ui/fragments/dialogfragments/WarningDialogFragment.kt
@@ -0,0 +1,22 @@
+package com.example.petpatandroidappdemo.ui.fragments.dialogfragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.petpatandroidappdemo.R
+
+
+class WarningDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_warning_dialog, container, false)
+    }
+
+}
