32b2bd1e6d7eeb86215056bfaf0ee2578ee695e5
Mehedi Hassan <mehedi.github@gmail.com>
2023-07-20 10:39:33 +0600

    updated

--- app/src/main/java/com/example/petpatandroidappdemo/ui/fragments/addproducts/AddProductFragment.kt
+++ app/src/main/java/com/example/petpatandroidappdemo/ui/fragments/addproducts/AddProductFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.petpatandroidappdemo.adapters.AddProductAdapter
 import com.example.petpatandroidappdemo.callbacks.AddProductItemSelectListener
@@ -35,15 +36,10 @@ class AddProductFragment : Fragment(), AddProductItemSelectListener, OptionDialo
     private lateinit var adapter: AddProductAdapter
     private lateinit var dialogFragment: ImageUploadOptionDialogFragment
     private var position = 0
-    private val ImageListRequestlist = mutableListOf<String>()
-
     private val imageList = ArrayList<RequestBody>()
-    private lateinit var headersMap: Map<String, String>
     private var count = 0
     private lateinit var loginResponse: LoginResponseModel
     private lateinit var uri: RequestBody
-    val imageFields = HashMap<String, String>()
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,47 +53,17 @@ class AddProductFragment : Fragment(), AddProductItemSelectListener, OptionDialo
 
         val token = arguments?.getString("accessToken").toString()
         Log.e("TAG", "token $token")
-
-
-        /*   headersMap =  HashMap<>()
-           headersMap.put("Authorization", "Bearer " + "5ac58dd5377e11f57eeb87fa8f9cca7fdd4fbeca33703257a243c6a9f0653726");
-   */
         adapter = AddProductAdapter(Constants.imageList(), this)
         binding.rvAddProduct.layoutManager =
             LinearLayoutManager(view.context, LinearLayoutManager.HORIZONTAL, false)
 
         binding.rvAddProduct.adapter = adapter
 
-
-
-
         binding.btnSave.setOnClickListener {
-
 
             val productName = binding.etProductName.text.toString()
             val productPrice = binding.etProductPrice.text.toString()
 
-
-            /* val addProductRequestModel =
-                 AddProductRequestModel(list, productName, productPrice)
- */
-            /* RetrofitClient.getService().getData("Bearer $token").enqueue(object : Callback<DataResponse> {
-                 override fun onResponse(
-                     call: Call<DataResponse>,
-                     response: Response<DataResponse>
-                 ) {
-                     Log.e("TAG", "before response ${response.body()} token $token")
-
-                     if (response.isSuccessful) {
-                         Log.e("TAG", "response ${response.body()}")
-                     }
-                 }
-
-                 override fun onFailure(call: Call<DataResponse>, t: Throwable) {
-                     TODO("Not yet implemented")
-                 }
-
-             })*/
 
             RetrofitClient.getService()
 
@@ -111,6 +77,7 @@ class AddProductFragment : Fragment(), AddProductItemSelectListener, OptionDialo
                             "TAG",
                             "response ${response.body()} size ${imageList.size}"
                         )
+                        // Toast.makeText(requireContext(), "size = ${response.body()?.data?.images?.size}").show()
                     }
 
                     override fun onFailure(call: Call<AddProductResponseModel>, t: Throwable) {
@@ -140,14 +107,9 @@ class AddProductFragment : Fragment(), AddProductItemSelectListener, OptionDialo
 
     override fun getImageUrl(position: Int, url: String) {
         count++
-
         val imageFile = File(url)
         val uri = imageFile.asRequestBody("image/jpeg".toMediaType())
         imageList.add(uri)
-        //uri = RequestBody.create("image/*".toMediaType(), imageFile)
-
-        //val imageRequestBody = imageFile.asRequestBody("image/*".toMediaTypeOrNull())
-        //imageFields["image[$count]\"; filename=\"${imageFile.name}"] = imageRequestBody.toString()
     }
 
 }
